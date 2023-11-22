cde063f94dbd05557139ba738710cbe179ad5a0c
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-11-22 14:48:28 -0300

    feat: added pipe to validate schema

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,27 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common'
+import { ZodError, ZodSchema } from 'zod'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      this.schema.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          error: error.format(),
+          message: 'Validation failed',
+          statusCode: 400,
+        })
+      }
+
+      throw new BadRequestException('Validation failed')
+    }
+    return value
+  }
+}
