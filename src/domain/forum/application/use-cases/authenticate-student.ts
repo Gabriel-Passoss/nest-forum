bcc2cd6e199cacd6df3c379c29da2387214201bc
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-11-23 18:14:37 -0300

    feat: create student entity, repository, use cases, errors handling, factory, contracts and tests for it

---
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -0,0 +1,55 @@
+import { Either, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { StudentsRepository } from '../repositories/students-repository'
+import { HashComparer } from '../crypto/hash-comparer'
+import { Encrypter } from '../crypto/encrypter'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+
+interface AuthenticateStudentUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateStudentUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+@Injectable()
+export class AuthenticateStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
+    const student = await this.studentsRepository.findByEmail(email)
+
+    if (!student) {
+      throw new WrongCredentialsError()
+    }
+
+    const isPasswordValid = this.hashComparer.compare(
+      password,
+      student.password,
+    )
+
+    if (!isPasswordValid) {
+      throw new WrongCredentialsError()
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: student.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
