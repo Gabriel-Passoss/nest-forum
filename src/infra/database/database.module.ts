b3f66c9de1f58901f545425aa38ef08870f96d52
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-11-23 15:16:16 -0300

    feat: adapted use case and repository to create a question for nest

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,33 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: QuestionsRepository,
+      useClass: PrismaQuestionsRepository,
+    },
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaQuestionCommentsRepository,
+    QuestionsRepository,
+  ],
 })
 export class DatabaseModule {}
--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -3,8 +3,8 @@ import { CurrentUser } from '@/infra/auth/current-user.decorator'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { UserPayload } from '@/infra/auth/jwt.strategy'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { z } from 'zod'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
@@ -18,7 +18,7 @@ const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly createQuestion: CreateQuestionUseCase) {}
 
   @Post()
   async handle(
@@ -27,27 +27,12 @@ export class CreateQuestionController {
   ) {
     const { title, content } = body
     const userId = user.sub
-    const slug = this.generateSlug(title)
 
-    await this.prisma.question.create({
-      data: {
-        authorId: userId,
-        title,
-        content,
-        slug,
-      },
+    await this.createQuestion.execute({
+      title,
+      content,
+      authorId: userId,
+      attachmentsIds: [],
     })
-  }
-
-  private generateSlug(title: string): string {
-    const withoutAccents = title
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-    const slug = withoutAccents
-      .toLowerCase()
-      .replace(/[^a-z0-9]+/g, '-')
-      .replace(/(^-|-$)+/g, '')
-
-    return slug
   }
 }
