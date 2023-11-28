9217799be72ba9bd26ce2127894c0463ff8cefca
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-11-28 17:02:39 -0300

    feat: create delete question controller and test for it

---
+++ src/infra/http/controllers/delete-question.controller.e2e-spec.ts
@@ -0,0 +1,58 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('Delete question (E2E)', () => {
+  let app: INestApplication
+  let jwt: JwtService
+  let prisma: PrismaService
+  let accessToken: string
+  let questionFactory: QuestionFactory
+  let studentFactory: StudentFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [QuestionFactory, StudentFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    jwt = moduleRef.get(JwtService)
+    prisma = moduleRef.get(PrismaService)
+    questionFactory = moduleRef.get(QuestionFactory)
+    studentFactory = moduleRef.get(StudentFactory)
+
+    await app.init()
+  })
+
+  it('Should be able to delete a question', async () => {
+    const user = await studentFactory.makePrismaStudent()
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const questionId = question.id.toString()
+
+    accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .delete(`/questions/${questionId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+
+    expect(response.statusCode).toBe(204)
+
+    const questionOnDatabase = await prisma.question.findUnique({
+      where: {
+        id: questionId,
+      },
+    })
+
+    expect(questionOnDatabase).toBeNull()
+  })
+})
