dd87c68a51e41f4c298d55851441852a20051ab6
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-11-28 15:41:41 -0300

    refactor: get question by slug to use make question and make student factories

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,56 +1,53 @@
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { AppModule } from '@/infra/app.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { DatabaseModule } from '@/infra/database/database.module'
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
 
 describe('Get question by slug (E2E)', () => {
   let app: INestApplication
-  let prisma: PrismaService
   let jwt: JwtService
   let accessToken: string
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
-    prisma = moduleRef.get(PrismaService)
     jwt = moduleRef.get(JwtService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
 
     await app.init()
 
-    const user = await prisma.user.create({
-      data: {
-        name: 'Jhon Doe',
-        email: 'jhondoe@email.com',
-        password: '123456',
-      },
+    const user = await studentFactory.makePrismaStudent()
+
+    await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+      title: 'Question 01',
+      slug: Slug.create('question-01'),
     })
 
-    await prisma.question.create({
-      data: {
-        title: 'Test question',
-        content: 'Testing to create a new question',
-        slug: 'test-question',
-        authorId: user.id,
-      },
-    })
-
-    accessToken = jwt.sign({ sub: user.id })
+    accessToken = jwt.sign({ sub: user.id.toString() })
   })
 
   it('Should be able to get a question by slug', async () => {
     const response = await request(app.getHttpServer())
-      .get('/questions/test-question')
+      .get('/questions/question-01')
       .set('Authorization', `Bearer ${accessToken}`)
 
     expect(response.statusCode).toBe(200)
 
     expect(response.body).toEqual({
-      question: expect.objectContaining({ title: 'Test question' }),
+      question: expect.objectContaining({ title: 'Question 01' }),
     })
   })
 })
